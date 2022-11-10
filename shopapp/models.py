032ecdcc42da65f1aa2f9669b8d732062d7f6539
Saparmurat <saparmurat@protonmail.com>
2022-11-10 16:43:22 +0600

    create models: Cart, CartItem, Order,  User

--- shopapp/models.py
+++ shopapp/models.py
@@ -1,6 +1,56 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_staff(self, email, password):
+        user = self.create_user(email, password)
+
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+
+        user.is_staff = True
+
+        user.is_admin = True
+
+        user.save(using=self._db)
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+            max_length=128,
+            unique=True)
+
+    phone_number = models.CharField()
+
+    name = models.CharField(max_length=100, blank=False, null=False)
+
+    is_admin = models.BooleanField(default=False, blank=True, null=False)
+    is_staff = models.BooleanField(default=False, blank=True, null=False)
+
+    USERNAME_FIELD = "email"
+
+    objects = UserManager
 
 
 class Address(models.Model):
@@ -12,12 +62,8 @@ class Address(models.Model):
             null=False,
             help_text="Street name or Building number")
     flat_number = models.IntegerField(blank=False, null=False)
-    # TODO
-    # add user as foreign key
 
-
-class User(AbstractBaseUser):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Category(models.Model):
@@ -33,7 +79,7 @@ class Product(models.Model):
             blank=False,
             null=False,
             help_text="Title of the product")
-    description = models.CharField(
+    description = models.TextField(
                 max_length=1000,
                 blank=False,
                 null=False,
@@ -56,8 +102,8 @@ class Product(models.Model):
     is_stock = models.BooleanField(blank=False, null=False, default=True)
 
     discount = models.FloatField(blank=True, default=.0, null=True)
-    # TODO
-    # add user as foreign key
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
@@ -77,5 +123,26 @@ class Comment(models.Model):
     rating = models.IntegerField(choices=ratings)
     creation_date = models.DateTimeField(auto_now=True, blank=False)
 
-    # TODO
-    # add user as foreign key
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Cart(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+
+    total_price = models.FloatField(blank=False, null=False)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    quantity = models.IntegerField(blank=False, null=False)
+
+    price = models.FloatField(blank=False, null=False)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
