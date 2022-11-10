3caa1e8cb2e2bf83dcdec5d145e909798a39f298
Saparmurat <saparmurat@protonmail.com>
2022-11-10 15:44:53 +0600

    create models: Product, Comment, Category, Address

--- shopapp/models.py
+++ shopapp/models.py
@@ -1,3 +1,81 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
+
+
+class Address(models.Model):
+    city = models.CharField(max_length=30, blank=False, null=False)
+    microdistrict = models.CharField(max_length=30, blank=False, null=False)
+    street = models.CharField(
+            max_length=30,
+            blank=False,
+            null=False,
+            help_text="Street name or Building number")
+    flat_number = models.IntegerField(blank=False, null=False)
+    # TODO
+    # add user as foreign key
+
+
+class User(AbstractBaseUser):
+    pass
+
+
+class Category(models.Model):
+    name = models.CharField(
+            max_length=20, blank=False,
+            null=False, unique=True)
+
+
+class Product(models.Model):
+
+    title = models.CharField(
+            max_length=100,
+            blank=False,
+            null=False,
+            help_text="Title of the product")
+    description = models.CharField(
+                max_length=1000,
+                blank=False,
+                null=False,
+                help_text="Description of a product")
+
+    pictures = models.CharField(
+            max_length=100,
+            help_text="Link to image location")
+
+    category = models.ForeignKey(
+                Category,
+                on_delete=models.DO_NOTHING,
+                blank=True,
+                null=True)
+
+    creation_date = models.DateTimeField(auto_now=True, blank=False)
+
+    price = models.FloatField(blank=False, null=False)
+    quantity = models.IntegerField(blank=False, null=False)
+    is_stock = models.BooleanField(blank=False, null=False, default=True)
+
+    discount = models.FloatField(blank=True, default=.0, null=True)
+    # TODO
+    # add user as foreign key
+
+
+class Comment(models.Model):
+
+    content = models.CharField(max_length=200, blank=False, null=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    replies = models.ForeignKey("self", on_delete=models.CASCADE)
+
+    ratings = [
+        (1, "1")
+        (2, "2")
+        (3, "3")
+        (4, "4")
+        (5, "5")
+    ]
+
+    rating = models.IntegerField(choices=ratings)
+    creation_date = models.DateTimeField(auto_now=True, blank=False)
+
+    # TODO
+    # add user as foreign key
