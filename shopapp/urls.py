0526def8e600a29e4f9b549b7347241ae3f6b276
Saparmurat <saparmurat@protonmail.com>
2022-11-15 13:57:06 +0600

    login, refresh register urls

--- shopapp/urls.py
+++ shopapp/urls.py
@@ -1,8 +1,29 @@
 from django.urls import path
 from shopapp import views
 
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 urlpatterns = [
     path('', views.ListProduct.as_view(), name='products'),
     path('user/<int:pk>', views.RetrieveUser.as_view(), name='user'),
+    path(
+        'product/create',
+        views.CreateProduct.as_view(),
+        name='create_product'
+    ),
+
+    path('category', views.ListCreateCategory.as_view(), name='category'),
+
+    path(
+        'login/',
+        views.CustomObtainTokenPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+    path(
+        'login/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
+    path('register/', views.RegisterView.as_view(), name='auth_register'),
 ]
