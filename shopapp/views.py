805bce7181a299c86119af13e42bf9853bf46d6b
Saparmurat <saparmurat@protonmail.com>
2022-11-15 13:57:25 +0600

    token, register views

--- shopapp/views.py
+++ shopapp/views.py
@@ -1,4 +1,8 @@
 from rest_framework import generics
+from rest_framework import permissions, authentication
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
 from .permissions import (
     ProductCommentPermissions
 )
@@ -7,33 +11,42 @@ from .models import (
     User,
     Product,
     # Address, Cart,
-    # CartItem, Category,
+    # CartItem,
+    Category,
     Comment,
     # Order
 )
 
 from .serializers import (
-    UserSerializer, CategorySerializer,
+    RegistrationSerializer, CategorySerializer,
     ProductSerializer, AddressSerializer,
     CartSerializer, CartItemSerializer,
-    CommentSerializer, OrderSerializer
+    CommentSerializer, OrderSerializer,
+    CustomTokenObtainPairSerializer,
+    UserSerializer,
 )
+
+
+class CustomObtainTokenPairView(TokenObtainPairView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = CustomTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    permission_classes = [permissions.AllowAny]
+
+    serializer_class = RegistrationSerializer
 
 
 class CreateAddress(generics.CreateAPIView):
     serializer_class = AddressSerializer
 
 
-class CreateCategory(generics.CreateAPIView):
-    serializer_class = CategorySerializer
-
-
 class CreateProduct(generics.CreateAPIView):
     serializer_class = ProductSerializer
 
-
-class CreateUser(generics.CreateAPIView):
-    serializer_class = UserSerializer
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class CreateCart(generics.CreateAPIView):
@@ -51,7 +64,14 @@ class AddCartItem(generics.CreateAPIView):
 class ListProduct(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [ProductCommentPermissions]
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ListCreateCategory(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = CategorySerializer
 
 
 class ListCreateComments(generics.ListCreateAPIView):
