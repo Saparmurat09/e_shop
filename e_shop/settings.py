e2d5338381f64dc3ba26939eec3b02049f57d8ff
Saparmurat <saparmurat@protonmail.com>
2022-11-17 01:28:37 +0600

    temporary commenting

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -90,10 +90,10 @@ if config('DEPLOYMENT') == True:
         'default': {
             'ENGINE': config('DB_ENGINE'),
             'NAME': config('DB_NAME'),
-            'USER': config('DB_USER'),
-            'PASSWORD': config('DB_PASSWORD'),
-            'HOST': config('DB_HOST', 'localhost'),
-            'PORT': '',
+            # 'USER': config('DB_USER'),
+            # 'PASSWORD': config('DB_PASSWORD'),
+            # 'HOST': config('DB_HOST', 'localhost'),
+            # 'PORT': '',
         }
     }
 else:
