646293b1521769db3e434b6d0e7883f989fe7c88
Saparmurat <saparmurat@protonmail.com>
2022-11-16 22:46:36 +0600

    set jwt token lifetime

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,3 +138,10 @@ REST_FRAMEWORK = {
             'rest_framework.permissions.IsAuthenticated',
         ]
     }
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(hours=24),
+    'ROTATE_REFRESH_TOKENS': True,
+}
